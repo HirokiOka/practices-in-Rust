9a987f329a9824ba25d86adc87307591e5c57306
hiroka <mementoka@gmail.com>
2023-03-09 23:01:58 +0900

    trpl 6

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,15 +4,24 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    println!("rect1 is {:?}", rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
